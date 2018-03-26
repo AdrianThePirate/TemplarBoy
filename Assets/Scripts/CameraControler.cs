26ab6db63f5babecc4e30d8092371b922b7e18f2
AdrianThePirates <ingadr15@student.westerdals.no>
2018-03-27 00:21:18 +0200

    Addad basic camera follow
    Camera now follow character.

---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+	public Transform PlayerTransform;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		var x = PlayerTransform.position.x;
+		var y = PlayerTransform.position.y;
+		if (x < 0)
+			x = 0;
+		if (y < 0)
+			y = 0;
+		transform.position = new Vector3(x, y, -10);
+	}
+}
