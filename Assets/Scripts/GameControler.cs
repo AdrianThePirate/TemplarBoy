ca5dc6baa9f4946ad19b2d09fe2ba1d6dce73934
AdrianThePirates <ingadr15@student.westerdals.no>
2018-03-26 15:38:36 +0200

    End screen
    Added an end screen with Ariel font (sorry not sorry)

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class GameControler : MonoBehaviour {
+	public static bool gameEnded { get; private set; }
+	public GameObject endScreen;
+	public static GameObject gameOver;
+
+	public static void EndGame() {
+		if (!gameEnded) {
+			gameEnded = true;
+			gameOver.SetActive(gameEnded);
+		}
+	}
+
+	void Start() {
+		gameEnded = false;
+		gameOver = endScreen;
+	}
+
+	void Update() {
+		if (gameEnded && Input.GetAxis("Jump") > 0)
+			ResetGame();
+		gameOver.SetActive(gameEnded);
+	}
+
+	void ResetGame() { 
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
