d106015692a76ac1ba8568eff15cde56dec87706
AdrianThePirate <adrian.techy@gmail.com>
2019-02-01 15:10:15 +0100

    Added Parallaxing
    Added Paralaxing

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+	public float paralaxEffectX = 0.1f;
+	public float paralaxEffectY = 0.1f;
+
+	private float lastCameraX;
+	private float lastCameraY;
+	private Transform cameraTansform;
+    // Start is called before the first frame update
+    void Start()
+    {
+		cameraTansform = Camera.main.transform;
+		lastCameraX = cameraTansform.position.x;
+		lastCameraY = cameraTansform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float deltaX = cameraTansform.position.x - lastCameraX;
+		transform.position += Vector3.right * (deltaX * paralaxEffectX);
+		lastCameraX = cameraTansform.position.x;
+
+		float deltaY = cameraTansform.position.y - lastCameraY;
+		transform.position += Vector3.up * (deltaY * paralaxEffectY);
+		lastCameraY = cameraTansform.position.y;
+	}
+}
